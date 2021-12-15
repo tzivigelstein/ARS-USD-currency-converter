924fcb95b2d430114ef66514edcabecb6910d7ad
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-15 15:38:55 -0300

    Change naming, parsing and improve performance

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [dolar, setDolar] = useState({})
-  const [toconvert, setToConvert] = useState({
-    monto: 0,
+  const [toConvert, setToConvert] = useState({
+    amount: 0,
   })
 
-  const resultInitialState = { pre: 0, impuesto1: 0, impuesto2: 0, final: 0 }
+  const resultInitialState = { preTotal: 0, firstTax: 0, secondTax: 0, finalAmount: 0 }
   const [result, setResult] = useState(resultInitialState)
 
-  const { monto } = toconvert
+  const { amount } = toConvert
   const { preTotal = 0, firstTax = 0, secondTax = 0, finalAmount = 0 } = result
 
   useEffect(() => {
@@ -17,17 +17,17 @@ const App = () => {
       getDolar()
     }
 
-    if (monto !== 0) {
-      compute(monto)
+    if (amount !== 0) {
+      compute(amount)
     }
-  }, [monto])
+  }, [amount])
 
   const getDolar = async () => {
     const values = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales').then(res => res.json())
 
     const parsedValues = values.map(({ casa: { compra, venta, agencia } }) => ({
-      compra: compra.replace(',', '.'),
-      venta: venta.replace(',', '.'),
+      compra: parseFloat(compra.replace(',', '.')),
+      venta: parseFloat(venta.replace(',', '.')),
       agencia,
     }))
 
@@ -38,7 +38,7 @@ const App = () => {
     e.preventDefault()
 
     setToConvert({
-      ...toconvert,
+      ...toConvert,
       [e.target.name]: e.target.value,
     })
   }
@@ -46,18 +46,18 @@ const App = () => {
   const compute = amount => {
     let { compra: price } = dolar
     const parsedAmount = parseFloat(amount)
-    const parsedPrice = parseFloat(price)
+    const parsedPrice = price
 
-    const preTotal = parsedPrice * parsedAmount
-    const firstTax = preTotal * 0.3
-    const secondTax = preTotal * 0.35
-    const f = preTotal + firstTax + secondTax
+    const preTotal = isNaN(parsedPrice * parsedAmount) ? 0 : parsedPrice * parsedAmount
+    const firstTax = isNaN(preTotal * 0.3) ? 0 : preTotal * 0.3
+    const secondTax = isNaN(preTotal * 0.35) ? 0 : preTotal * 0.35
+    const finalAmount = isNaN(preTotal + firstTax + secondTax) ? 0 : preTotal + firstTax + secondTax
 
     setResult({
-      preTotal: preTotal.toFixed(2),
-      firstTax: firstTax.toFixed(2),
-      secondTax: secondTax.toFixed(2),
-      finalAmount: f.toFixed(2),
+      preTotal: preTotal.toLocaleString([], { currency: 'ARS', maximumFractionDigits: 2 }),
+      firstTax: firstTax.toLocaleString([], { currency: 'ARS', maximumFractionDigits: 2 }),
+      secondTax: secondTax.toLocaleString([], { currency: 'ARS', maximumFractionDigits: 2 }),
+      finalAmount: finalAmount.toLocaleString([], { currency: 'ARS', maximumFractionDigits: 2 }),
     })
   }
 
@@ -67,15 +67,15 @@ const App = () => {
         <div className="w-4/5 sm:w-2/3 bg-white mt-32">
           <p className="text-center text-3xl m-4">
             <span className="text-green-600">
-              {dolar.compra} <span className="text-xl">ARS</span>
+              {dolar.compra || 0} <span className="text-xl">ARS</span>
             </span>{' '}
             = 1 <span className="text-xl">USD</span>
           </p>
           <form className="flex justify-center items-center">
             <input
-              name="monto"
+              name="amount"
               onChange={onChange}
-              value={monto}
+              value={amount}
               type="number"
               className="w-2/3 py-2 px-3 text-gray-700 bg-gray-200"
               placeholder="Monto a convertir"
@@ -100,9 +100,6 @@ const App = () => {
       <footer className="bg-white w-4/5 sm:w-2/3 m-auto">
         <div className="p-3">
           <p className="text-center text-gray-600 mt-4">Conversor de dolar a pesos para compras al exterior</p>
-          <p className="text-center text-gray-600 mt-2">
-            Copyright {new Date().getFullYear()} Todos los derechos reservados &copy;
-          </p>
         </div>
       </footer>
     </div>
